5230e94860909e98f632cc1ca2caf3841051dba8
Jorge Ramirez <jwramirezc@gmail.com>
2025-03-05 18:59:44 -0500

    se agrega archivo de configuracion

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+// 1. Archivo: config.js - Define las imágenes por cliente
+const clienteImagenes = {
+  cliente1:
+    'https://www.saiasoftware.com/wp-content/uploads/2024/imagenesdemos/logo_portal_demo1.png',
+  cliente2:
+    'https://www.saiasoftware.com/wp-content/uploads/2024/imagenesdemos/logo_portal_demo2.png',
+  cliente3:
+    'https://www.saiasoftware.com/wp-content/uploads/2024/imagenesdemos/logo_portal_demo3.png',
+  default:
+    'https://www.saiasoftware.com/wp-content/uploads/2024/imagenesdemos/logo_portal_saia.png',
+};
